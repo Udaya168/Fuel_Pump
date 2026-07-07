c2e5b0c97af924ba7ff723ed24625259e4431cb6
Rudrik <rudrik28novjoshi@gmail.com>
2026-07-07 09:40:29 +0530

    Migrate to native nitro server build

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+import { nitro } from "nitro/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [
+    nitro(),
     tanstackStart(),
     react(),
     tailwindcss(),
